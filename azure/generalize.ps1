2c3acf8ed8c286bb55f2f57ffb67b7aac2f8d8e7
Olaf Gradin <olaf.gradin@gmail.com>
2022-09-26 06:33:05 -0400

    Accepting Azure Marketplace terms for new TKG VM (#55)
    tkg-capi: k8s-1dot23dot8-ubuntu-2004
    2_dns vars modify subnet name to match 1_netsec user_subnet.tf naming.
    PowerShell cleanup automation "generalize.ps1" used for packaging takes
    out-null commands for tasks which should be done through a Terraform
    destroy step previously.
    
    Signed-off-by: Olaf Gradin <ogradin@vmware.com>
    
    Signed-off-by: Olaf Gradin <ogradin@vmware.com>
    Co-authored-by: Olaf Gradin <ogradin@vmware.com>

--- azure/generalize.ps1
+++ azure/generalize.ps1
@@ -2,13 +2,13 @@ Remove-Item -Path .\0_keepers\terraform.tfstate*
 Remove-Item -Path .\0_keepers\.terraform.lock.hcl
 Remove-Item -Recurse -Path .\0_keepers\.terraform -Force
 Remove-Item -Path .\1_netsec\.terraform.lock.hcl
-Remove-Item -Path .\1_netsec\provider.tf
+Remove-Item -Path .\1_netsec\provider.tf | Out-Null
 Remove-Item -Recurse -Path .\1_netsec\.terraform -Force
 Remove-Item -Path .\2_dns\.terraform.lock.hcl
-Remove-Item -Path .\2_dns\provider.tf
+Remove-Item -Path .\2_dns\provider.tf | Out-Null
 Remove-Item -Recurse -Path .\2_dns\.terraform -Force
 Remove-Item -Path .\3_bootstrap\.terraform.lock.hcl
-Remove-Item -Path .\3_bootstrap\data.tf
-Remove-Item -Path .\3_bootstrap\provider.tf
+Remove-Item -Path .\3_bootstrap\data.tf | Out-Null
+Remove-Item -Path .\3_bootstrap\provider.tf | Out-Null
 Remove-Item -Recurse -Path .\3_bootstrap\.terraform -Force
-Remove-Item -Recurse -Path .\3_bootstrap\bootstrap.pem
+Remove-Item -Recurse -Path .\3_bootstrap\bootstrap.pem | Out-Null
